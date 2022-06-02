f5fc223cd9616812dcadffdb5ca49015bf664ca8
Sadid369 <sadid.jones@gmail.com>
2022-06-02 21:05:25 +0600

    2/6/2022

---
+++ main.py
@@ -0,0 +1,6 @@
+# file=  open("my_file.txt") = with open("my_file.txt") as file
+# with  open("my_file.txt") as file:
+#     content= file.read()
+#     print(content)
+with open("new_file.txt", mode="w") as file:
+    file.write("\nnew Text")
